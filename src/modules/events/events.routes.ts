0498013a77614e10dab7c8d262cfba0f68aece3b
GabrielPeixoto111 <gdpacademico@gmail.com>
2026-04-08 15:25:15 -0300

    fix: - Substituição do onRequest por preHandler no events.routes.ts

--- src/modules/events/events.routes.ts
+++ src/modules/events/events.routes.ts
@@ -29,19 +29,19 @@ export async function eventsRoutes(app: FastifyInstance) {
 
   api.post(
     '/events',
-    { schema: { body: createEventSchema }, onRequest: [app.authenticate] },
+    { schema: { body: createEventSchema }, preHandler: [app.authenticate] },
     postEvent,
   )
 
   api.put(
     '/events/:id',
-    { schema: { params: eventParamSchema, body: updateEventSchema } },
+    { schema: { params: eventParamSchema, body: updateEventSchema }, preHandler: [app.authenticate] },
     putEvent,
   )
 
   api.delete(
     '/events/:id',
-    { schema: { params: eventParamSchema } },
+    { schema: { params: eventParamSchema }, preHandler: [app.authenticate] },
     deleteEventHandler,
   )
 }
